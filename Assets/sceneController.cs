9c9053b53cc572db66a09048dbbca5a59ee5b4ea
BlitzBurn <39604601+BlitzBurn@users.noreply.github.com>
2018-11-05 10:30:19 +0100

    Forts Commit
    Added proto main menu and level 1

---
+++ Assets/sceneController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneController : MonoBehaviour {
+
+	public void sceneChanger(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
