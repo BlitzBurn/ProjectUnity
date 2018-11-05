04965fa6568ea912b746777d129f9bdc3d92f5d6
BlitzBurn <39604601+BlitzBurn@users.noreply.github.com>
2018-11-05 11:42:23 +0100

    Added player

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody2D;
+    bool grounded = true;
+    double previousYpos = 0;
+    
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        grounded = Physics2D.Raycast(rigidbody2D.position, new Vector2(0, -1), 20, -1);
+        print(grounded);
+
+        
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 7);
+        }
+
+        previousYpos = System.Math.Round(rigidbody2D.position.y, 2);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody2D;
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * 5;
+
+        rigidbody2D.velocity = new Vector2(x, rigidbody2D.velocity.y);
+    }
+}
