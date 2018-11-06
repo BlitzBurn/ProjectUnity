9207b5d6155791f2fe787596d4c697df5487ce60
BlitzBurn <39604601+BlitzBurn@users.noreply.github.com>
2018-11-06 12:16:27 +0100

    added level transition

---
+++ Assets/Scripts/SceneTransitionTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransitionTrigger : MonoBehaviour {
+
+   public string level;
+
+    private void OnTriggerEnter2D(Collider2D Colider)
+    {
+        if (Colider.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(level);
+        
+        }
+        
+    }
+}
