8e75d52bfe6af81bb865c7dc6dc7edb90a9350c7
Mabrima <31730695+Mabrima@users.noreply.github.com>
2018-11-06 10:48:42 +0100

    Simplistic first blink

---
+++ Assets/Scripts/PlayerBlink.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlink : MonoBehaviour {
+    public float blinkDistance;
+
+
+    Rigidbody2D rigidbody2D;
+    bool movingRight = false;
+    bool movingLeft = false;
+
+
+    // Use this for initialization
+    void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (rigidbody2D.velocity.x < 0)
+        {
+            movingLeft = true;
+            movingRight = false;
+        }
+        else if (rigidbody2D.velocity.x > 0)
+        {
+            movingLeft = false;
+            movingRight = true;
+        }
+
+		if (Input.GetMouseButtonDown(0))
+        {
+            if (movingLeft)
+            {
+                transform.position -= new Vector3(blinkDistance, 0, 0);
+            }
+            else if (movingRight)
+            {
+                transform.position += new Vector3(blinkDistance, 0, 0);
+            }
+        }
+	}
+}
