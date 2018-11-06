e278deb28d7eff94f91738b1be90c15612c7f86e
fiolot <sebastian_kba@hotmail.com>
2018-11-06 13:56:45 +0100

    Basic enemy interaction
    Idiot enemy movement, player to enemy interaction.

---
+++ Assets/Scripts/Enemy/EnemyGompa.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGompa : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public float movementSpeed;
+    private float direction = -1;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collide");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        direction *= -1;
+    }
+    private void Update()
+    {
+        rigidbody2D.velocity = new Vector2(direction * movementSpeed, rigidbody2D.velocity.y);
+    }
+}
